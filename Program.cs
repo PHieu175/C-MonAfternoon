f1791385803d8a3a624abd23a172a26c32e94bc6
Phan Nguyen Minh Hieu <phannguyenminhhieu.2007@gmail.com>
2026-08-16 23:03:13 +0700

    Them cac dong noi dung tuy y

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, Phan Hieu!");
+﻿Console.WriteLine("Hello, World!");
+  // In các dòng nội dung tùy ý
+            Console.WriteLine("Xin chao thay va cac ban!");
+            Console.WriteLine("Minh dang theo hoc mon Co so lap trinh.");
+            Console.WriteLine("Muc tieu: Nam vung tu duy logic va cu phap C#.");
