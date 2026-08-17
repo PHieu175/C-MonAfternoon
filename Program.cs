f6fb7034f498e610dc4923f59444b420cedf14ec
Phan Nguyen Minh Hieu <phannguyenminhhieu.2007@gmail.com>
2026-08-17 16:05:34 +0700

    bai tap moi tren lop

--- Program.cs
+++ Program.cs
@@ -1,5 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Hello, World!");
   // In các dòng nội dung tùy ý
             Console.WriteLine("Xin chao thay va cac ban!");
             Console.WriteLine("Minh dang theo hoc mon Co so lap trinh.");
             Console.WriteLine("Muc tieu: Nam vung tu duy logic va cu phap C#.");
+*/
+//-----Code bai buoi2 o day-----
+//to Add/Sum two numbers
+Console.Write("Enter the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = num1 + num2;
+
+Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+//to Swap Values of Two Variables.
+// 1. to Add / Sum Two Numbers.
+// 2. to Swap Values of Two Variables.
+// 3. to Multiply two Floating Point Numbers
+// 4. to convert feet to meter
+// 5. to convert Celsius to Fahrenheit and vice versa
+// 6. to find the Size of data types
+// 7. to Print ASCII Value (tip: read character, print number of this char)
+// 8. to Calculate Area of Circle
+// 9. to Calculate Area of Square
+// 10. to convert days to years, weeks and days
