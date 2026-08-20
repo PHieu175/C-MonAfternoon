affb21227e14a5a0ac0d7c83f5c03106c5b7e29e
Phan Nguyen Minh Hieu <phannguyenminhhieu.2007@gmail.com>
2026-08-20 22:37:06 +0700

    add ex 7, 8, 9, 10

--- Session02/Program.cs
+++ Session02/Program.cs
@@ -12,11 +12,11 @@ internal class Excercise_1
         Console.WriteLine($"{number1} + {number2} = {sum}");
 
         // 2. to Swap Values of Two Variables.
-        Console.WriteLine($"Before swap number 1 = {number1}, number 2 = {number2}");
+        Console.WriteLine($"Before swap number 1: = {number1}, number 2 = {number2}");
         int temp = number1;
         number1 = number2;
         number2 = temp;
-        Console.WriteLine($"After swap number 1 = {number1}, number 2 = {number2}");
+        Console.WriteLine($"After swap number 1: = {number1}, number 2 = {number2}");
 
         // 3. to Multiply two Floating Point Numbers
         float f1 = 3.5f, f2 = 4.7f;
@@ -30,16 +30,44 @@ internal class Excercise_1
         Console.WriteLine($"{feet} feet = {metter} metter");
 
         // 5. to convert Celsius to Fahrenheit and vice versa
+        float cels = 27f;
+        float fah = cels * 1.8f + 32;
+        Console.WriteLine($"{cels}°C = {fah}F");
 
         // 6. to find the Size of data types
-
+        Console.WriteLine($"Size of double data type is {sizeof(double)}");
+        Console.WriteLine($"Size of int data type is {sizeof(int)}");
+       
         // 7. to Print ASCII Value (tip: read character, print number of this char)
+        Console.Write("Enter a single character: ");
+        char myChar = Console.ReadKey().KeyChar;
+        Console.WriteLine(); // Moves the console cursor to the next line
+        int asciiValue = (int)myChar;
+        Console.WriteLine($"The ASCII value of '{myChar}' is {asciiValue}"); 
+
+        // THÊM DÒNG NÀY ĐỂ XÓA PHÍM ENTER BỊ KẸT:
+        Console.ReadLine(); 
 
         // 8. to Calculate Area of Circle
+        Console.Write("Enter the radius of the circle: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double circleArea = Math.PI * radius * radius;
+        Console.WriteLine($"The area of the circle is {circleArea}");
 
         // 9. to Calculate Area of Square
+        Console.Write("Enter the side length of the square: ");
+        double side = Convert.ToDouble(Console.ReadLine());
+        double squareArea = side * side;
+        Console.WriteLine($"The area of the square is {squareArea}");
 
         // 10. to convert days to years, weeks and days
+        Console.Write("Enter the total number of days: ");
+        int totalDays = Convert.ToInt32(Console.ReadLine());
+        int years = totalDays / 365;
+        int remainingDays = totalDays % 365;
+        int weeks = remainingDays / 7;
+        int days = remainingDays % 7;
+        Console.WriteLine($"{totalDays} days is equivalent to {years} years, {weeks} weeks, and {days} days.");
 
         Console.ReadKey();
 
