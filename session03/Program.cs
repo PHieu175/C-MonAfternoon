81140f597a63af74687d3ab91a5731cb47a1d848
Phan Nguyen Minh Hieu <phannguyenminhhieu.2007@gmail.com>
2026-08-21 15:53:56 +0700

    add bai tap buoi 3

---
+++ session03/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Excercise_21thAug
+{
+    
+public static void Main(string[]args)
+    {
+        //Bai 1:
+        Console.Write("Nhap chi so Dien cu (kWh): "); 
+        int chisoCu = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nhap chi so Dien moi (kWh): ");
+        int chisoMoi = Convert.ToInt32(Console.ReadLine());
+
+        decimal tienDienchuathue = 0;
+        decimal ThueVat = 0;
+        decimal tongTien = 0;
+
+        if(chisoMoi < chisoCu)
+        {
+            Console.WriteLine("Loi: chi so Moi phai lon hon chi so Cu!!!");
+            return;
+        }
+        int soDien = chisoMoi - chisoCu;
+
+        if(soDien < 50)
+        {
+            tienDienchuathue = soDien*1806m;
+        }
+        if(soDien < 100)
+        {
+            tienDienchuathue = soDien*1866m;
+        }
+        if(soDien < 200)
+        {
+            tienDienchuathue = soDien*2167m;
+        }
+        if(soDien<300)
+        {
+            tienDienchuathue = soDien*2729m;
+        }
+        else
+        {
+            tienDienchuathue = soDien*3050m;
+        }
+
+        ThueVat = tienDienchuathue*0.08m;
+        tongTien = tienDienchuathue + ThueVat;
+
+        Console.WriteLine($"So dien tieu thu: {soDien:N0} kWh");
+        Console.WriteLine($"Tien dien chua thue: {tienDienchuathue:N0} VND");
+        Console.WriteLine($"Thue VAT (8%): {ThueVat:N0} VND");
+        Console.WriteLine($"Tong thanh toan: {tongTien:N0} VND");
+
+        Console.ReadKey();
+
+    }
+}
